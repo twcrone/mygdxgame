e9087eef777004a1552eac37c723fced012100e0
Todd Crone <twcrone@gmail.com>
2020-10-18 16:56:10 -0400

    Remove unused artifacts

--- desktop/src/com/twcrone/mygdxgame/desktop/DesktopLauncher.java
+++ desktop/src/com/twcrone/mygdxgame/desktop/DesktopLauncher.java
@@ -5,11 +5,7 @@ import com.twcrone.mygdxgame.starfishcollector.StarfishCollectorBeta;
 
 public class DesktopLauncher {
   public static void main(String[] arg) {
-    //		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-    //		new LwjglApplication(new MyGdxGame(), config);
-    //		StarfishCollectorAlpha myProgram = new StarfishCollectorAlpha();
-    //		LwjglApplication launder = new LwjglApplication(myProgram, "Starfish Collector", 800, 600);
     StarfishCollectorBeta myProgram = new StarfishCollectorBeta();
-    LwjglApplication launder = new LwjglApplication(myProgram, "Starfish Collector", 800, 600);
+    new LwjglApplication(myProgram, "Starfish Collector", 800, 600);
   }
 }
