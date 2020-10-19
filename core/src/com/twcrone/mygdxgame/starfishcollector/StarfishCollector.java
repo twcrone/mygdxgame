c18e17df2a85faee247391c70694ab3c6e481c68
Todd Crone <twcrone@gmail.com>
2020-10-18 21:41:03 -0400

    Remove some unnecessary abstraction

--- core/src/com/twcrone/mygdxgame/starfishcollector/StarfishCollector.java
+++ core/src/com/twcrone/mygdxgame/starfishcollector/StarfishCollector.java
@@ -1,8 +1,12 @@
 package com.twcrone.mygdxgame.starfishcollector;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
-public class StarfishCollector extends GameBeta {
+public class StarfishCollector extends Game {
 
   private Turtle turtle;
 
@@ -10,7 +14,27 @@ public class StarfishCollector extends GameBeta {
   private Rock rock;
   private BaseActor ocean;
 
+  protected Stage mainStage;
+
   @Override
+  public void create() {
+    mainStage = new Stage();
+    initialize();
+  }
+
+  public void render() {
+    float dt = Gdx.graphics.getDeltaTime();
+
+    mainStage.act();
+
+    update(dt);
+
+    Gdx.gl.glClearColor(0, 0, 0, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+    mainStage.draw();
+  }
+
   public void initialize() {
     ocean = new BaseActor(0, 0, mainStage);
     ocean.loadTexture("water.jpg");
